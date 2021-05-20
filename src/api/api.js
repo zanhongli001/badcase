a3f7115a3b2c99555d5cb3953005e9602f78b98f
lijing18 <lijing18@360.cn>
2021-05-20 20:41:52 +0800

    feat: 1、add上传组件；2、完善新建底库数据；

--- src/api/api.js
+++ src/api/api.js
@@ -1,92 +1,91 @@
-import { fetchPost,fetchGet} from '../utils/request'
+import { fetchPost, fetchGet } from '@/utils/request'
 import qs from 'qs'
 // let loading =  this.$loading({
 //     lock: true,
 //   });
 // 底库数据列表
-export function getPolList(params){
-    return  fetchGet('/policy/task/',params).then((res) =>{
-return res.data.results
-    })
+export function getPolList(params) {
+  return fetchGet('/policy/task/', params).then((res) => {
+    return res.data.results
+  })
 }
 // 底库上传里分类请求
-export function getPolIfy(params){
-    return  fetchGet('policy/task_category/',params).then((res) =>{
-return res.data
-    })
+export function getPolify(params) {
+  return fetchGet('policy/task_category/', params).then((res) => {
+    return res.data
+  })
 }
 
 // 获取底库数据详情
-export function getPolDetail(params){
-    return  fetchGet(`/policy/task/${params}/`).then((res) =>{
-        console.log(res)
-return res.data
-    })
+export function getPolDetail(params) {
+  return fetchGet(`/policy/task/${params}/`).then((res) => {
+    console.log(res)
+    return res.data
+  })
 }
 // 提交底库数据详情
-export function subPolDetail(data){
-    console.log(data)
-    return fetchPost('/AIBaseAudit',data).then((res) => {
-return res
-    })
+export function subPolDetail(data) {
+  console.log(data)
+  return fetchPost('/AIBaseAudit', data).then((res) => {
+    return res
+  })
 }
 
-// 上传底库数据
-
-export function uplodPol(data){
-    return fetchPost('/UploadAIBaseData',data).then((res) => {
-        return res
-    })
+// 新建底库数据
+export function addPol(data) {
+  return fetchPost('/policy/task/', data).then((res) => {
+    return res
+  })
 }
 
 // 查询地库数据
-export function queryPol(params){
-    return fetchGet('/AIBaseDataList',params).then((res) =>{
-        return res.data.data
-    })
+export function queryPol(params) {
+  return fetchGet('/AIBaseDataList', params).then((res) => {
+    return res.data.data
+  })
 }
 // badcase列表
-export function getBadList(params){
-    return  fetchGet('badcase/task/',params).then((res) =>{
-        console.log(res)
-return res.data.results
-    })
+export function getBadList(params) {
+  return fetchGet('badcase/task/', params).then((res) => {
+    console.log(res)
+    return res.data.results
+  })
 }
 
 // 上传badcase数据
-export function uplodBad(data){
-    console.log(data)
-    return fetchPost('/AIBadCase',data).then((res) => {
-        console.log(res)
-        return res
-    })
+export function uplodBad(data) {
+  console.log(data)
+  return fetchPost('/AIBadCase', data).then((res) => {
+    console.log(res)
+    return res
+  })
 }
 
 // badcase 批量查看图片
 
-export function watchAllImg(params){
-    return fetchGet(`/AIBatchImages?task_id=${params}`).then((res) =>{
-        console.log(res.data.data.feedback_image)
-        return res.data.data.feedback_image
-    })
+export function watchAllImg(params) {
+  return fetchGet(`/AIBatchImages?task_id=${params}`).then((res) => {
+    console.log(res.data.data.feedback_image)
+    return res.data.data.feedback_image
+  })
 }
 
 // badcase 获取详情
 
-export function getCaseDetail(params){
-    return fetchGet(`/AIBadcaseFeedBackDetail?id=${params}`).then((res) =>{
-        console.log(res)
-        return res.data.data.ai_type
-    })
+export function getCaseDetail(params) {
+  return fetchGet(`/AIBadcaseFeedBackDetail?id=${params}`).then((res) => {
+    console.log(res)
+    return res.data.data.ai_type
+  })
 }
 
 // badcase 提交详情
 
 // 上传badcase数据
-export function subBadDetail(data){
-    console.log(data)
-    return fetchPost('/AIBadCaseFeedback',data).then((res) => {
-        console.log(res)
-        return res
-    })
+export function subBadDetail(data) {
+  console.log(data)
+  return fetchPost('/AIBadCaseFeedback', data).then((res) => {
+    console.log(res)
+    return res
+  })
 }
---
+++ src/components/Upload/index.vue
@@ -0,0 +1,72 @@
+<template>
+	<div class="m-upload-wrapper">
+		<el-upload
+			:action="action"
+			:multiple="multiple"
+			:accept="accept"
+			:limit="limit"
+			:maxSize="maxSize"
+			list-type="picture-card"
+			:on-preview="handlePreview"
+			:on-remove="handleRemove"
+			:on-success="handleSuccess"
+			:before-upload="beforeUpload"
+		>
+			<i class="el-icon-plus"></i>
+		</el-upload>
+		<el-dialog :visible.sync="previewVisible">
+			<img width="100%" :src="imageUrl" alt="" />
+		</el-dialog>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'upload',
+		props: {
+			action: {
+				type: String,
+				required: true,
+			},
+			multiple: {
+				type: Boolean,
+				default: true,
+			},
+			accept: {
+				type: String,
+				default: 'image/*',
+			},
+			limit: {
+				type: Number,
+				default: 50,
+			},
+			maxSize: {
+				type: Number,
+				default: 100,
+			},
+		},
+		data() {
+			return {
+				previewVisible: false,
+				imageUrl: '',
+			};
+		},
+		methods: {
+			handleRemove(file, fileList) {},
+			handlePreview(file) {
+				this.imageUrl = file.url;
+				this.previewVisible = true;
+			},
+			handleSuccess(response, file, fileList) {
+				let fileIds = fileList.map((item) => {
+					return item.response && item.response.id;
+				});
+				this.$emit('change', fileIds);
+			},
+			beforeUpload(file, fileList) {},
+			checkType(file) {},
+			checkSize(file) {},
+		},
+	};
+</script>
+<style lang="scss" scoped>
+</style>
